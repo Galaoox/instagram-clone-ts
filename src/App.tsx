6b864e14948b040d43111cf85cc0ff6b6d6e4525
Erick Vergara <erickandres30@gmail.com>
2020-07-23 21:50:47 -0500

    Created styles for the app__header

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello esto es una app con react y ts</h1>
-
+    <div className="app">
+      {/*  HEADER */}
       <div className="app__header">
-        <img className="" />
         <img alt="Instagram" className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" srcSet="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png 2x"></img>
       </div>
-      {/*  HEADER */}
-
+      <h1>Hello esto es una app con react y ts</h1>
       {/*  POSTS */}
+
       {/*  POSTS */}
 
     </div>
