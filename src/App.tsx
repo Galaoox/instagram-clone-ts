1bc7eba6a7985f21df586a275f047f9ebdf33958
Erick Vergara <erickandres30@gmail.com>
2020-07-23 21:56:58 -0500

    Divide the components post and header of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="app">
       {/*  HEADER */}
-      <div className="app__header">
-        <img alt="Instagram" className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" srcSet="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png 2x"></img>
-      </div>
+      <Header />
       <h1>Hello esto es una app con react y ts</h1>
       {/*  POSTS */}
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+    return (
+        <div className="app__header">
+            <img alt="Instagram" className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" srcSet="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png 2x"></img>
+        </div>
+    )
+}
+
+
+export default Header;
